a7e7f8d2b2d86efeccdf4e3d558451d953b8941b
Génesis <bellmon90@hotmail.com>
2021-02-22 14:21:40 -0700

    Genesis/ nuevo commit

--- venv-caterra/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\rene\desktop\pitic\proyectos\caterra\venv-caterra\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
