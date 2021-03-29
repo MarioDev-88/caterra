2ac363183ac946f1100342d0b8ff9fd464cb08e6
Jero Guzman <jeroguzman@gmail.com>
2021-03-29 13:51:25 -0700

    update base.py

--- src/caterra/settings/base.py
+++ src/caterra/settings/base.py
@@ -123,14 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATICFILES_DIRS = [str(BASE_DIR / 'static')]
+STATIC_URL = "/static_root/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Media files
-#MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_ROOT = [str(BASE_DIR / 'media')]
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 
